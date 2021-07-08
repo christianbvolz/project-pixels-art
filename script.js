f18bda2a6cb3e0c1ac294f4cb8f0eef103301b2d
Christianbv <christianbvolz@gmail.com>
2021-07-08 16:07:20 -0300

    requisito 2

---
+++ script.js
@@ -0,0 +1,9 @@
+function colorPalette() {
+  const palette = document.querySelectorAll('.color');
+  palette[0].style.backgroundColor = 'green';
+  palette[1].style.backgroundColor = 'red';
+  palette[2].style.backgroundColor = 'blue';
+  palette[3].style.backgroundColor = 'yellow';
+}
+
+colorPalette();
